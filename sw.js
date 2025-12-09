d0793b0a5e700232a5ca42d2b4f48d6019375812
itoren <46642805+itoren@users.noreply.github.com>
2025-12-09 11:50:01 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+// sw.js
+const CACHE_NAME = 'mycall-v7';
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
